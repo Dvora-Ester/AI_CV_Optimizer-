4ac1c877f64a82c02550bc1edab74ea33f48ac5e
Bracha222 <b0583251390@gmail.com>
2025-11-24 19:01:23 +0200

    cv download server

--- server/app.js
+++ server/app.js
@@ -3,12 +3,16 @@ import cors from "cors";
 import dotenv from "dotenv";
 import cvGeneralRoutes from "./routes/cvGeneral.routes.js";
 import cvJobRoutes from "./routes/cvJob.routes.js";
+import downloadRoutes from "./routes/download.js";
+
+
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
 app.use((req, res, next) => {
 	const now = new Date().toISOString();
 	console.log(`${now} -> ${req.method} ${req.originalUrl} - Content-Type: ${req.headers['content-type'] || ''}`);
@@ -16,5 +20,6 @@ app.use((req, res, next) => {
 });
 app.use("/api/cv/general", cvGeneralRoutes);
 app.use("/api/cv/job", cvJobRoutes);
+app.use("/api/download", downloadRoutes);
 //'http://localhost:3000/api/cv/optimize/general'
 export default app;
